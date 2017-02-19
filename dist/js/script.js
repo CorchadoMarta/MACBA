abad5d58fb0713703635c14d98d6ea5b71bb7d58
Alimachaves <quintero.cami@gmail.com>
2017-02-19 10:35:37 +0100

    fotos dist

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $("#navToggle").click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $(".navbar ul").slideToggle( "slow");
+    });
+
+    $('.maps').click(function () {
+    $('.maps iframe').css("pointer-events", "auto");
+    });
+
+    $( ".maps" ).mouseleave(function() {
+      $('.maps iframe').css("pointer-events", "none");
+    });
+});
