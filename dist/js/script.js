d4add2c95959507b6e14f8e6db968d57b33c48b0
Marta Corchado <gcatram@gmail.com>
2017-05-06 16:43:20 +0200

    carrito compra

--- dist/js/script.js
+++ dist/js/script.js
@@ -49,5 +49,18 @@ $(document).ready(function(){
 				return false;
 			});
 
-		});
+    });
+    // display the first div by default.
+    $("#accordion div").first().css('display', 'block');
+    // Get all the links.
+    var link = $("#accordion a");
+    // On clicking of the links do something.
+    link.on('click', function(e) {
+      e.preventDefault();
+      var a = $(this).attr("href");
+      $(a).slideDown('fast');
+      //$(a).slideToggle('fast');
+      $("#accordion div").not(a).slideUp('fast');
+      $(".crossRotate").toggleClass('active');
+    });
 });
