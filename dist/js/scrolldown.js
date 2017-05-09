861e24950bb45d42a3c03b79a8d17997477e7a38
Marta Corchado <gcatram@gmail.com>
2017-05-09 21:18:43 +0200

    arreglos

--- dist/js/scrolldown.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-
-  $('.scroll-down').click (function() {
-    $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
-    return false;
-  });
-});
