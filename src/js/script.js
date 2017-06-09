de300e0e2b0984cbabd5ac8c9b99810a1e6fe2f3
Alimachaves <quintero.cami@gmail.com>
2017-06-09 18:34:03 +0200

    formulario y idiomas

--- src/js/script.js
+++ src/js/script.js
@@ -196,3 +196,13 @@ jQuery(document).ready(function ($) {
   });
 
 });
+
+
+$(function () {
+    $el_languages = $('.languages');
+    $el_languages.hover(function () {
+        $el_languages.find('li').show();
+    }, function () {
+        $el_languages.find('li:not(.active)').hide();
+    });
+});
